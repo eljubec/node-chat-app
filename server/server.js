1ff714ac4029f89c70d4e134a052dccf009fae17
Elias Ljubec <35690245+evoqqq@users.noreply.github.com>
2018-03-08 12:46:31 +0100

    add connect, disconnect event

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const path = require('path');
-const hbs = require('hbs');
-const express = require('express')
-let app = express();
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const pulbicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+let app = express();
+// configer the server to use socket.io - thats the reason why we switched to http
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(pulbicPath));
 
-app.listen(port, () => {
+
+io.on('connect', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconected from server')
+    });
+    socket.on('')
+});
+
+
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 });
