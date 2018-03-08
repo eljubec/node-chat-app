c87695e7d9b0d37c81d2c8b7a244d709e3bd5c36
Elias Ljubec <35690245+evoqqq@users.noreply.github.com>
2018-03-08 10:03:59 +0100

    Setting up the Server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const hbs = require('hbs');
+const express = require('express')
+let app = express();
+
+const pulbicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(pulbicPath));
+
+app.listen(3000, () => {
+    console.log(`Server is up on port ${port}`)
+});
